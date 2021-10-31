a6a4188e2f2ed443e6615c4a7fa0a9d85577f673
Oğuzhan Varsak <oguzhan.varsak@turkmedya.com.tr>
2021-10-31 13:08:48 +0300

    Initial Commit

---
+++ XCode Releases/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCode Releases
+//
+//  Created by Oğuzhan Varsak on 31.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
